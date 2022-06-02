0a1975614626ce1c606cd316eed8500de7c62da0
Ahangar <fenay001@ucr.edu>
2022-06-02 01:33:07 -0700

    build added to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -67,3 +67,7 @@ function watchTask() {
 
 // Default Gulp Task
 exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+// Build Gulp Task
+exports.build = series(scssTask, jsTask);
+
